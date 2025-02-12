9df4e180b466389ec9e9987337af4489590959a5
dlwjdtjq001 <dlwjdtjq001@gmail.com>
2025-02-12 15:45:25 +0900

    Feat : comparable implemet

--- src/day08/collection/sorting/student/StudentService.java
+++ src/day08/collection/sorting/student/StudentService.java
@@ -65,9 +65,6 @@ public class StudentService {
             rank++;
         }
         return rankNameMap;
-
     }
-
-
 
 }
--- src/day08/collection/sorting/student/studentMain.java
+++ src/day08/collection/sorting/student/studentMain.java
@@ -13,7 +13,9 @@ public class studentMain {
 
         data.add(studentService.createStudent("오나라",studentService.createScores(99,99,99)));
         data.add(studentService.createStudent("홍길동",studentService.createScores(54,65,53)));
-        data.add(studentService.createStudent("김선달",studentService.createScores(43,53,98)));
+        data.add(studentService.createStudent("김선달",studentService.createScores(43,53,94)));
+        data.add(studentService.createStudent("강낭콩",studentService.createScores(43,53,68)));
+        data.add(studentService.createStudent("곽두식",studentService.createScores(43,53,88)));
 
 
         System.out.println("1번 : 수학정렬 / 2번: 영어정렬 / 3번: 과학정렬 / 4번:총점정렬 / 5번: 랭킹출력");
